6d07b0debf1a8c0bb1a03c553b8ff78afc176f32
kseniamenshikova <ksenka.me@yandex.ru>
2019-04-06 12:44:44 +0300

    init commit

--- table.py
+++ table.py
@@ -1,11 +1,11 @@
 value = input("Введите атомный номер: ")
 if value:
         Z = int(value)
-        if Z == 3:
-            print("Литий")
-        elif 25:
-            print("Марганец")
-        elif 80:
-            print("Серебро")
-        elif 17:
-            print("Хлор")
+if Z == 3:
+        print("Литий")
+if Z == 25:
+       print("Марганец")
+if Z == 80:
+        print("Серебро")
+if Z == 17:
+        print("Хлор")
